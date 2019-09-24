cde479bbb4dcf52b6220cb22f0f0da6804d7d342
MAX_BLOOM <bloommax3@gmail.com>
2019-09-23 20:27:38 -0400

    added links to the portfolio page

---
+++ logic.js
@@ -0,0 +1,18 @@
+$("#giphySearch").on("click", function(){
+    window.location.href = "https://bloommax3.github.io/giphy-ajax/"
+})
+
+$("#psychicGame").on("click", function(){
+    window.location.href = "https://bloommax3.github.io/Psychic-Game/"
+})
+
+$("#gemGame").on("click", function(){
+    window.location.href = "https://bloommax3.github.io/unit-4-games/"
+})
+
+$("#trainSheet").on("click", function(){
+    window.location.href = "https://bloommax3.github.io/train-activity/"
+})
+$("#triviaGame").on("click", function(){
+    window.location.href = "https://bloommax3.github.io/TriviaGame/"
+})
